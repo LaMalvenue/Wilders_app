df8d1dd6a3c8e01eafa66e550bae7165a85a27a5
Anaïs Rouvière <anais.rouviere@kobalt.fr>
2020-11-04 23:22:07 +0100

    test avec react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,35 +2,33 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import "./App.css";
-import wilders from './data';
 import { Wilder } from "./components/wilder/wilder";
+import wilders from "./data.json";
 
 function App() {
   return (
     <div>
       <header>
-        <Container>
           <Row>
-            <Col>
+            <Col md>
               <h1>Wilders Book</h1>
             </Col>
           </Row>
-        </Container>
       </header>
-      <Container>
-        <Row>
-          <Col>
+      <body>
+        <Container fluid>
+          <Row>
             <h2>Wilders</h2>
-          </Col>
-        </Row>
-        <Row>
-          {wilders.map(data => (
-            <Col xs={3} className="mb-5" key={`${data.id}`}>
-              <Wilder data={data}/>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+          </Row>
+          <Row>
+            {wilders.map(data => (
+              <Col md={4} className="mb-5" key={`{data.id}`}>
+                <Wilder data={data}/>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </body>
       <footer>
         <div className="container">
           <p>&copy; 2020 Wild Code School</p>
--- src/components/wilder/wilder.js
+++ src/components/wilder/wilder.js
@@ -1,27 +1,36 @@
-import blank_profile from "../../blank-profile-picture-female.png";
 import "./wilder.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
 import { Skill } from "../skill/skill";
-import { Card } from "react-bootstrap";
+import data from "../../data.json";
 
-export const Wilders = ({ data }) => {
+export const Wilder = () => {
+    const skills = [
+        { "name": "HTML", "votes": 5 },
+        { "name": "CSS", "votes": 8 },
+        { "name": "JS", "votes": 3 },
+        { "name": "Java", "votes": 7 },
+        { "name": "PHP", "votes": 9 }
+    ];
     return (
-        <Card className="shadow-sm bg-white rounded">
-            <Card.Img src={data.image} alt="Jane Doe Profile" />
-            <Card.Body className="d-flex flex-column">
-                <div className="d-flex mb-2 justify-content-beetween">
-                    <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
-                </div>
-                <Card.Text className="text-secondary">
-                    {data.description}
-                </Card.Text>
-                <Card.Title>Wild Skills</Card.Title>
-                <Card.Text className="skills">
-                    {
-                        skills.map(({name, votes}, index) => <Skill name={name} votes={votes} key={index}/>)
-                    }
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <div>
+            <Card className="shadow-sm bg-white rounded">
+                <Card.Img variant="top" src={data.image} alt="Jane Doe Profile" />
+                <Card.Body className="d-flex flex-column">
+                    <div className="d-flex mb-2 justify-content-beetween">
+                        <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
+                    </div>
+                    <Card.Text className="text-secondary">
+                        {data.description}
+                    </Card.Text>
+                    <Card.Title>Wild Skills</Card.Title>
+                    <Card.Text className="skills">
+                        {
+                            skills.map(({name, votes}, index) => <Skill name={name} votes={votes} key={index}/>)
+                        }
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
     );
-}
-export default Wilders;
+};
